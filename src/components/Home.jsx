ed708288dd6be746ca6f05da92b60087edd3ad7f
StaticTJO <sk8terboy6931@gmail.com>
2018-04-12 17:43:58 -0600

    Added BootStrap Table skeleton.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,15 @@
 import React,{Component} from 'react'
 import {Link} from 'react-router-dom';
 import {Jumbotron,Grid,Row,Col,Image,Button} from 'react-bootstrap';
-
 import './Home.css';
+import Table from './Table';
+
+var data = [
+    {id: 1, name: 'Gob', value: '2'},
+    {id: 2, name: 'Buster', value: '5'},
+    {id: 3, name: 'George Michael', value: '4'}
+];
+
 
 export default class Home extends Component{
 
@@ -12,7 +19,10 @@ export default class Home extends Component{
                 <Jumbotron>
                     <h2>Welcome to Pet Weater App</h2>
                 </Jumbotron>
-
+                <div>
+                    <p className="Table-header"> Pet List </p>
+                    <Table data ={data}/>
+                </div>
             </Grid>
         )
     }
---
+++ src/components/Table.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import './Table.css';
+
+class Table extends Component {
+    render() {
+        return (
+            <div>
+                <BootstrapTable data={this.props.data}>
+                    <TableHeaderColumn isKey dataField='id'>
+                        ID
+                    </TableHeaderColumn>
+                    <TableHeaderColumn dataField='name'>
+                        Name
+                    </TableHeaderColumn>
+                    <TableHeaderColumn dataField='value'>
+                        Value
+                    </TableHeaderColumn>
+                </BootstrapTable>
+            </div>
+        );
+    }
+}
+
+export default Table;
