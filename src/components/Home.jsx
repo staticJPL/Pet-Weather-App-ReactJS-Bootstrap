4040ff67bc71d84b046fd37ea1d8cc4c305cd821
StaticTJO <sk8terboy6931@gmail.com>
2018-04-13 01:08:05 -0600

    Pushing here to before dealing with bug from Webpack.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,13 @@
 import React,{Component} from 'react'
 import {Link} from 'react-router-dom';
-import {Jumbotron,Grid,Row,Col,Image,Button} from 'react-bootstrap';
+import {Jumbotron,Grid,Button} from 'react-bootstrap';
 import './Home.css';
 import Table from './Table';
 
-var data = [
-    {id: 1, name: 'Gob', value: '2'},
-    {id: 2, name: 'Buster', value: '5'},
-    {id: 3, name: 'George Michael', value: '4'}
-];
+
 
 
 export default class Home extends Component{
-
     render(){
         return(
             <Grid>
@@ -21,7 +16,7 @@ export default class Home extends Component{
                 </Jumbotron>
                 <div>
                     <p className="Table-header"> Pet List </p>
-                    <Table data ={data}/>
+                    <Table/>
                 </div>
             </Grid>
         )
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,22 +3,46 @@ import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import './Table.css';
 
 class Table extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            petData:[],
+        };
+    }
+
+    componentWillMount(){
+        // Got this Get request finally working, needed to setup Heroku this took hours to figure out, never knew this even existed
+        fetch('http://localhost:8000/pets')
+            .then(results=>{
+                return results.json();
+            }).then(data=>{
+
+                this.setState({petData:data.response});
+                //console.log(this.state.petData.length);
+        })
+
+    }
+
+    CreateTable(){
+
+        function buttonFormatter(cell, row){
+            return '<Button type="submit">View</Button>';
+        }
+
+        return(
+            <BootstrapTable data={this.state.petData} hover={true}>
+                <TableHeaderColumn dataField="pet_lookup_id" isKey={true} dataAlign="center" >PetID</TableHeaderColumn>
+                <TableHeaderColumn dataField="name"> Pet Name</TableHeaderColumn>
+                <TableHeaderColumn dataField="type">Type</TableHeaderColumn>
+                <TableHeaderColumn dataField="breed">Breed</TableHeaderColumn>
+                <TableHeaderColumn dataField="button" dataFormat={buttonFormatter}></TableHeaderColumn>
+        </BootstrapTable>);
+    }
+
     render() {
-        return (
-            <div>
-                <BootstrapTable data={this.props.data}>
-                    <TableHeaderColumn isKey dataField='id'>
-                        ID
-                    </TableHeaderColumn>
-                    <TableHeaderColumn dataField='name'>
-                        Name
-                    </TableHeaderColumn>
-                    <TableHeaderColumn dataField='value'>
-                        Value
-                    </TableHeaderColumn>
-                </BootstrapTable>
-            </div>
-        );
+
+        return (this.CreateTable());
     }
 }
 
